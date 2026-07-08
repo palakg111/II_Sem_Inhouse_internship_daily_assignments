3a79d81eec187ea27d0d2ff49180b324c118b722
palakg111 <palakpradeep250@gmail.com>
2026-07-08 12:57:23 +0530

    Add registration form in index.php

---
+++ 25ESKCX074/DAY 10/index.php
@@ -0,0 +1,35 @@
+<form action="insert.php" method="POST">
+
+<div class="mb-3">
+<label>Name</label>
+<input type="text" name="name" class="form-control" required>
+</div>
+
+<div class="mb-3">
+<label>Email</label>
+<input type="email" name="email" class="form-control" required>
+</div>
+
+<div class="mb-3">
+<label>Phone</label>
+<input type="text" name="phone" class="form-control" required>
+</div>
+
+<div class="mb-3">
+<label>Photo Filename</label>
+<input type="text" name="photo" class="form-control">
+</div>
+
+<div class="mb-3">
+<label>Address</label>
+<textarea name="address" class="form-control"></textarea>
+</div>
+
+<div class="mb-3">
+<label>Course</label>
+<input type="text" name="course" class="form-control">
+</div>
+
+<button type="submit" class="btn btn-primary">Register</button>
+
+</form>
